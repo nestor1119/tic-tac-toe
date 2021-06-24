0dafdd20541290e1648383c7b9612cd7780c0b04
nestor1119 <nestorgfortique@gmail.com>
2021-06-24 02:07:39 +0200

    Update main.cpp

--- tic tac toe/main.cpp
+++ tic tac toe/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 char grid [3][3] = {'1','2','3','4','5','6','7','8','9'};
-char player = 'x';
+char spiller = 'x';
 void display(){
     for (int i = 0;i<3;i++){
         for (int j=0;j<3;j++){
@@ -13,6 +13,40 @@ void display(){
 
 }
 void input(){
+    int plass;
+cout<<"legg inn nr av plassen du vil spille på" <<endl;
+cin >> plass;
+switch(plass){
+    case 1:
+        matrix[0][0] = spiller;
+        break;
+    case 2:
+        matrix[0][1] = spiller;
+        break;
+    case 3:
+        matrix[0][2] = spiller;
+        break;
+    case 4:
+        matrix[1][0] = spiller;
+        break;
+    case 5:
+        matrix[1][1] = spiller;
+        break;
+    case 6:
+        matrix[1][2] = spiller;
+        break;
+    case 7:
+        matrix[2][0] = spiller;
+        break;
+    case 8:
+        matrix[2][1] = spiller;
+        break;
+    case 9:
+        matrix[2][2] = spiller;
+        break;
+
+}
+
 }
 
 int main()
